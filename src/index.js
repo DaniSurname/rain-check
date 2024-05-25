d901009ab1a6b2e87147f884c5db8a5b72f2d75c
DaniSurname <daniharvey25@gmail.com>
2024-05-25 17:59:17 +0100

    Added search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,37 @@
-function updateInfo(event) {
+function updateInfo(response) {
+  // Updates city name element
+  let nameElement = document.querySelector("#name");
+  nameElement.innerHTML = response.data.city;
+  // Updates comment element
+  let commentElement = document.querySelector("#comment");
+  let comment = response.data.condition.description;
+  commentElement.innerHTML = comment.toUpperCase();
+  // Updates temperature element
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+  // Updates humidity element
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  // Updates wind element
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = response.data.wind.speed;
+}
+// console.log(comment);
+
+function searchCity(city) {
+  let apiKey = "384f9t60d2a80f6f159b0a59do1ee44d";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateInfo);
+}
+function getCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
   let nameElement = document.querySelector("#name");
-  nameElement.innerHTML = searchInput.value;
-  //   console.log(searchInput.value);
+  searchCity(searchInput.value);
 }
 
 let searchBar = document.querySelector("#search-bar");
-searchBar.addEventListener("submit", updateInfo);
+searchBar.addEventListener("submit", getCity);
+
+searchCity("London");
